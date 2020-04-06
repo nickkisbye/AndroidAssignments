9a9ca5d4ec294e00687d1aa8c625a7c4caf1c6fd
Nick Hansen <44166062+nickkisbye@users.noreply.github.com>
2020-04-06 17:03:24 +0200

    Comments

--- Week 14/src/main/java/com/example/picturefirestorage/MainActivity.java
+++ Week 14/src/main/java/com/example/picturefirestorage/MainActivity.java
@@ -23,8 +23,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         emailText = findViewById(R.id.emailText);
         passwordText = findViewById(R.id.passwordText);
-        authRepository = new AuthRepository(this);
+        authRepository = new AuthRepository(this); 
+        //Dependency injection, so we can use the methods from this class in the AuthRepository.
     }
+    
+    /**
+    When Main activity starts, the AuthRepository will be instanciated and check if the user is logged in. 
+    If user login is successful, onLoginSuccess will be called and go to the PictureUploadActivity.
+    */
 
     public void onLoginSuccess(FirebaseUser user) {
         Intent intent = new Intent(this, PictureUploadActivity.class);
@@ -32,10 +38,17 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    /**
+    Just for printing out the message, can be used to show an error message to the user in the future.
+    */
     public void onLoginFail() {
         System.out.println("Logged out");
     }
 
+    /**
+    When the Login button is pressed, this method will be called. The signUserIn method from authRepository
+    will take this activity as the third parameter so it can call the OnLoginSuccess method if user authentication is succesful.
+    */
     public void signIn(View view) {
         String email = emailText.getText().toString();
         String password = passwordText.getText().toString();
